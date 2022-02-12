54493b42234e8bca0c669a384e0d81aa1b5fc2d5
David Calabrese <david.thomas.calabrese@gmail.com>
2022-02-11 19:13:30 -0600

    add getters / setters

--- src/main/java/org/davidcalabrese/entity/Post.java
+++ src/main/java/org/davidcalabrese/entity/Post.java
@@ -18,4 +18,94 @@ public class Post {
         this.timeCreated = timeCreated;
         this.userId = userId;
     }
+
+    /**
+     * Gets the value of <code>id</code>
+     *
+     * @return value of <code>id</code>
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * Sets the value of <code>id</code>
+     *
+     * @param id the value of <code>id</code>
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * Gets the value of <code>title</code>
+     *
+     * @return value of <code>title</code>
+     */
+    public String getTitle() {
+        return title;
+    }
+
+    /**
+     * Sets the value of <code>title</code>
+     *
+     * @param title the value of <code>title</code>
+     */
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    /**
+     * Gets the value of <code>content</code>
+     *
+     * @return value of <code>content</code>
+     */
+    public String getContent() {
+        return content;
+    }
+
+    /**
+     * Sets the value of <code>content</code>
+     *
+     * @param content the value of <code>content</code>
+     */
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    /**
+     * Gets the value of <code>timeCreated</code>
+     *
+     * @return value of <code>timeCreated</code>
+     */
+    public LocalDate getTimeCreated() {
+        return timeCreated;
+    }
+
+    /**
+     * Sets the value of <code>timeCreated</code>
+     *
+     * @param timeCreated the value of <code>timeCreated</code>
+     */
+    public void setTimeCreated(LocalDate timeCreated) {
+        this.timeCreated = timeCreated;
+    }
+
+    /**
+     * Gets the value of <code>userId</code>
+     *
+     * @return value of <code>userId</code>
+     */
+    public int getUserId() {
+        return userId;
+    }
+
+    /**
+     * Sets the value of <code>userId</code>
+     *
+     * @param userId the value of <code>userId</code>
+     */
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
 }
