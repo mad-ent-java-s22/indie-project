59e9bd955a3be1260fa7560e9d8adb893146342c
David Calabrese <david.thomas.calabrese@gmail.com>
2022-02-11 19:12:44 -0600

    add instance fields and constructors

--- src/main/java/org/davidcalabrese/entity/Post.java
+++ src/main/java/org/davidcalabrese/entity/Post.java
@@ -1,4 +1,21 @@
 package org.davidcalabrese.entity;
 
+import java.time.LocalDate;
+
 public class Post {
+    private int id;
+    private String title;
+    private String content;
+    private LocalDate timeCreated;
+    private int userId;
+
+    public Post() {}
+
+    public Post(int id, String title, String content, LocalDate timeCreated, int userId) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+        this.timeCreated = timeCreated;
+        this.userId = userId;
+    }
 }
