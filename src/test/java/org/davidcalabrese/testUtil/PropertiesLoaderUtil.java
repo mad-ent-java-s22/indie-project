1cdaf7fdaf56776631ab1a81a2d669842a6ccd4a
David Calabrese <david.thomas.calabrese@gmail.com>
2022-02-16 16:51:37 -0600

    create properties loader interface for test util

---
+++ src/test/java/org/davidcalabrese/testUtil/PropertiesLoaderUtil.java
@@ -0,0 +1,32 @@
+package org.davidcalabrese.testUtil;
+
+import java.util.Properties;
+
+/**
+ * This interface contains a default method that can be used anywhere a Properties
+ * object is needed to be loaded.
+ * @author Eric Knapp
+ *
+ */
+public interface PropertiesLoaderUtil {
+    /**
+     * This default method will load a properties file into a Properties instance
+     * and return it.
+     *
+     * @param propertiesFilePath a path to a file on the java classpath list
+     * @return a populated Properties instance or an empty Properties instance if
+     * the file path was not found.
+     * @throws Exception something went wrong accessing the properties file
+     */
+    default Properties loadProperties(String propertiesFilePath) throws Exception {
+        Properties properties = new Properties();
+        try {
+            properties.load(this.getClass().getResourceAsStream(propertiesFilePath));
+        } catch (Exception ioException) {
+            ioException.printStackTrace();
+            throw ioException;
+        }
+        return properties;
+    }
+}
+
