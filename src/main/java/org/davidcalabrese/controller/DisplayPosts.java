91c4cf7dbc5b3e1e441dc89c13b8dd7d522260de
David Calabrese <david.thomas.calabrese@gmail.com>
2022-02-21 18:12:01 -0600

    add servlet to display all blog posts

--- src/main/java/org/davidcalabrese/controller/DisplayPosts.java
+++ src/main/java/org/davidcalabrese/controller/DisplayPosts.java
@@ -1,2 +1,28 @@
-package org.davidcalabrese.controller;public class DisplayPosts {
+package org.davidcalabrese.controller;
+
+import org.davidcalabrese.entity.Post;
+import org.davidcalabrese.persistence.GenericDao;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet(name = "DisplayPosts", urlPatterns = { "/posts" })
+public class DisplayPosts extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String url = "/jsp/posts.jsp";
+        GenericDao<Post> postDao = new GenericDao<>(Post.class);
+
+        List posts = postDao.getAll();
+
+        req.setAttribute("posts", posts);
+        RequestDispatcher dispatcher = getServletContext().getRequestDispatcher(url);
+        dispatcher.forward(req, resp);
+    }
 }
