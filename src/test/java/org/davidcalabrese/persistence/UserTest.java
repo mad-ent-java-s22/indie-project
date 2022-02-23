3f76276e763c114de83021036008f7865fd1fd10
David Calabrese <david.thomas.calabrese@gmail.com>
2022-02-23 11:21:55 -0600

    build setup

--- src/test/java/org/davidcalabrese/persistence/UserTest.java
+++ src/test/java/org/davidcalabrese/persistence/UserTest.java
@@ -1,2 +1,22 @@
-package org.davidcalabrese.persistence;public class UserTest {
+package org.davidcalabrese.persistence;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.davidcalabrese.entity.User;
+import org.davidcalabrese.testUtil.Database;
+import org.junit.jupiter.api.BeforeEach;
+
+public class UserTest {
+    GenericDao userDao;
+
+    private final Logger logger = LogManager.getLogger(this.getClass());
+
+    @BeforeEach
+    void setUp() {
+        logger.info("setting up new user test");
+        userDao = new GenericDao<User>(User.class);
+
+        Database database = Database.getInstance();
+        database.runSQL("cleandb.sql");
+    }
 }
