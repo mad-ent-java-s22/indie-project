409d07a273c7a22960251984c072a19c2ee2c699
David Calabrese <david.thomas.calabrese@gmail.com>
2022-02-24 12:54:43 -0600

    add summary column ddl and inserts

--- src/test/resources/cleandb.sql
+++ src/test/resources/cleandb.sql
@@ -11,6 +11,7 @@ CREATE TABLE post (
                       content text NOT NULL,
                       date_created timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
                       user_id int NOT NULL,
+                      summary varchar(255) NOT NULL,
                       CONSTRAINT post_pk PRIMARY KEY (id)
 ) COMMENT 'represents a blog post';
 
@@ -74,36 +75,44 @@ INSERT INTO `blog`.`tag` (`name`) VALUES ('funny');
 INSERT INTO `blog`.`tag` (`name`) VALUES ('sports');
 
 -- inserts 5 test posts, each with a tag or two
-INSERT INTO `blog`.`post` (`title`, `content`, `user_id`) VALUES ('Post 1', 'This is post 1, it is about politics and education', 1);
+INSERT INTO `blog`.`post` (`title`, `content`, `user_id`, `summary`) VALUES ('Post 1', 'This is post 1, it is about politics and education', 1, 'This is post 1, it is about politics and education');
 INSERT INTO `blog`.`post_tag` (`tag_id`, `post_id`) VALUES (1, last_insert_id());
 INSERT INTO `blog`.`post_tag` (`tag_id`, `post_id`) VALUES (2, last_insert_id());
 
-INSERT INTO `blog`.`post` (`title`, `content`, `user_id`) VALUES ('Post 2', 'ESTANCIA, N.M. — The chorus of small voices ringing from a third-grade classroom on a recent morning signaled how far Estancia Elementary School had come in resuming a sense of normalcy after the latest coronavirus surge.
+INSERT INTO `blog`.`post` (`title`, `content`, `user_id`, `summary`) VALUES ('Substitutes in Camouflage',
 
-Students in the small, remote community of Estancia, N.M., were enthusiastically engaged in a vocabulary lesson, enunciating words with a “bossy r,” as well as homophones and homonyms, and spelling them on white boards.
-
-But there was also a sign of how far the district, about an hour outside Albuquerque, still had to go. The teacher moving about the classroom and calling on students to use the words in a sentence was clad in camouflage. “My substitute is wearing gear,” one student responded.
-
-“Yes,” Lt. Col. Susana Corona replied, beaming. “The superintendent allows me to wear my uniform. I’m wearing a pair of boots.”
-
-For the last month, dozens of soldiers and airmen and women in the New Mexico National Guard have been deployed to classrooms throughout the state to help with crippling pandemic-related staff shortages. Gov. Michelle Lujan Grisham has also enlisted civilian state employees — herself included — to volunteer as substitute teachers.
-
-New Mexico has been the only state to deploy National Guard troops in classrooms. But since the fall, when districts around the country began recruiting any qualified adult to take over classrooms temporarily, several other states have turned to uniformed personnel. National Guard members in Massachusetts have driven school buses, and last month, police officers in one city in Oklahoma served as substitutes.
-
-The scenes of uniformed officers in classrooms have solicited mixed reactions. Some teachers see it as a slight against their profession, and a way to avoid tackling longstanding problems like low teacher pay. Other critics have worried that putting more uniformed officers in schools could create anxiety in student populations that have historically had hostile experiences with law enforcement.', 2);
+                '<p>
+                  ESTANCIA, N.M. — The chorus of small voices ringing from a third-grade classroom on a recent morning signaled how far Estancia Elementary School had come in resuming a sense of normalcy after the latest coronavirus surge.
+                </p>
+                <p>
+                  Students in the small, remote community of Estancia, N.M., were enthusiastically engaged in a vocabulary lesson, enunciating words with a “bossy r,” as well as homophones and homonyms, and spelling them on white boards.
+                </p>
+                <p>
+                  But there was also a sign of how far the district, about an hour outside Albuquerque, still had to go. The teacher moving about the classroom and calling on students to use the words in a sentence was clad in camouflage. “My substitute is wearing gear,” one student responded.
+                </p>
+                <p>
+                    “Yes,” Lt. Col. Susana Corona replied, beaming. “The superintendent allows me to wear my uniform. I’m wearing a pair of boots.”</p>
+                </p>
+                <p>
+                  For the last month, dozens of soldiers and airmen and women in the New Mexico National Guard have been deployed to classrooms throughout the state to help with crippling pandemic-related staff shortages. Gov. Michelle Lujan Grisham has also enlisted civilian state employees — herself included — to volunteer as substitute teachers.
+                </p>
+                <p>
+                New Mexico has been the only state to deploy National Guard troops in classrooms. But since the fall, when districts around the country began recruiting any qualified adult to take over classrooms temporarily, several other states have turned to uniformed personnel. National Guard members in Massachusetts have driven school buses, and last month, police officers in one city in Oklahoma served as substitutes
+                </p>',
+                                                                  2, 'Deployed to classrooms in New Mexico, National Guard troops employ their informal motto, “Semper Gumby” — Always Flexible.');
 INSERT INTO `blog`.`post_tag` (`tag_id`, `post_id`) VALUES (2, last_insert_id());
 
-INSERT INTO `blog`.`post` (`title`, `content`, `user_id`) VALUES ('Post 3', 'This is post 3, it is about movies', 3);
+INSERT INTO `blog`.`post` (`title`, `content`, `user_id`, `summary`) VALUES ('Post 3', 'This is post 3, it is about movies', 3, 'This is post 3, it is about movies');
 INSERT INTO `blog`.`post_tag` (`tag_id`, `post_id`) VALUES (3, last_insert_id());
 
-INSERT INTO `blog`.`post` (`title`, `content`, `user_id`) VALUES ('Post 4', 'This is post 4, it is about literature', 2);
+INSERT INTO `blog`.`post` (`title`, `content`, `user_id`, `summary`) VALUES ('Post 4', 'This is post 4, it is about literature', 2, 'This is post 4, it is about literature');
 INSERT INTO `blog`.`post_tag` (`tag_id`, `post_id`) VALUES (4, last_insert_id());
 
-INSERT INTO `blog`.`post` (`title`, `content`, `user_id`) VALUES ('Post 5', 'This is post 5, it is about entertainment and sports', 4);
+INSERT INTO `blog`.`post` (`title`, `content`, `user_id`, `summary`) VALUES ('Post 5', 'This is post 5, it is about entertainment and sports', 4, 'This is post 5, it is about entertainment and sports');
 INSERT INTO `blog`.`post_tag` (`tag_id`, `post_id`) VALUES (5, last_insert_id());
 INSERT INTO `blog`.`post_tag` (`tag_id`, `post_id`) VALUES (10, last_insert_id());
 
-INSERT INTO `blog`.`post` (`title`, `content`, `user_id`) VALUES ('Post 6', 'This is post 6, it is about politics', 4);
+INSERT INTO `blog`.`post` (`title`, `content`, `user_id`, `summary`) VALUES ('Post 6', 'This is post 6, it is about politics', 4, 'This is post 6, it is about politics');
 INSERT INTO `blog`.`post_tag` (`tag_id`, `post_id`) VALUES (1, last_insert_id());
 
 -- End of file.
