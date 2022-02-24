a8ff5f1f3b78e8570007338da62853c229c15163
David Calabrese <david.thomas.calabrese@gmail.com>
2022-02-23 20:01:59 -0600

    add tests

--- src/test/java/org/davidcalabrese/persistence/UserTest.java
+++ src/test/java/org/davidcalabrese/persistence/UserTest.java
@@ -2,9 +2,15 @@ package org.davidcalabrese.persistence;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.davidcalabrese.entity.Post;
 import org.davidcalabrese.entity.User;
 import org.davidcalabrese.testUtil.Database;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class UserTest {
     GenericDao userDao;
@@ -18,5 +24,19 @@ public class UserTest {
 
         Database database = Database.getInstance();
         database.runSQL("cleandb.sql");
+    }
+
+    @Test
+    public void getAllUsersSuccess() {
+        logger.info("in getAllUsersSuccess");
+        List<User> users = userDao.getAll();
+        assertEquals(4, users.size());
+    }
+
+    @Test
+    public void getUserByIdSuccess() {
+        logger.info("in getUserByIdSuccess");
+        User user1 = (User) userDao.getById(1);
+        assertEquals("Pam", user1.getFirstName());
     }
 }
