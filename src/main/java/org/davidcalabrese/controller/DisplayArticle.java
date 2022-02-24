f2f48c2b95d8be8a17b4f0a18dc046b0de995683
David Calabrese <david.thomas.calabrese@gmail.com>
2022-02-24 12:55:40 -0600

    add servlet for displaying individual articles

--- src/main/java/org/davidcalabrese/controller/DisplayArticle.java
+++ src/main/java/org/davidcalabrese/controller/DisplayArticle.java
@@ -1,2 +1,27 @@
-package org.davidcalabrese.controller;public class DisplayArticle {
+package org.davidcalabrese.controller;
+
+import org.davidcalabrese.entity.Post;
+import org.davidcalabrese.persistence.GenericDao;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "DisplayArticle", urlPatterns = { "/article" })
+public class DisplayArticle extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String url = "/jsp/article.jsp";
+        GenericDao<Post> postDao = new GenericDao<>(Post.class);
+
+        Post post = postDao.getById(2);
+
+        req.setAttribute("post", post);
+        RequestDispatcher dispatcher = getServletContext().getRequestDispatcher(url);
+        dispatcher.forward(req, resp);
+    }
 }
