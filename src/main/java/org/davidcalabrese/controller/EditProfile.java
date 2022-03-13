87ebb14022930ba19a409aa3280d749e7aa8df1f
David Calabrese <david.thomas.calabrese@gmail.com>
2022-03-13 11:33:01 -0500

    add logic to insert user from EditProfile servlet

--- src/main/java/org/davidcalabrese/controller/EditProfile.java
+++ src/main/java/org/davidcalabrese/controller/EditProfile.java
@@ -1,16 +1,30 @@
 package org.davidcalabrese.controller;
 
 import org.davidcalabrese.entity.User;
+import org.davidcalabrese.persistence.GenericDao;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 public class EditProfile extends HttpServlet  {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        User newUser = new User();
+        // get userName and email that from cognito, stored in session
+        HttpSession session = req.getSession();
+        String userName = (String) session.getAttribute("userName");
+        String email = (String) session.getAttribute("email");
+        GenericDao<User> userDao = new GenericDao<>(User.class);
+
+
+        User newUser = new User(userName, email);
+        newUser.setFirstName(req.getParameter("first_name"));
+        newUser.setLastName(req.getParameter("last_name"));
+        newUser.setSummary(req.getParameter("about"));
+
+        userDao.insert(newUser);
     }
 }
