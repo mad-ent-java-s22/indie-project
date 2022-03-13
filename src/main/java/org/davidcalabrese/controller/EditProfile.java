9c4a6b56f51dbec5e6fac2927c157ae7bab44d28
David Calabrese <david.thomas.calabrese@gmail.com>
2022-03-13 11:52:35 -0500

    save newly created user in session scope

--- src/main/java/org/davidcalabrese/controller/EditProfile.java
+++ src/main/java/org/davidcalabrese/controller/EditProfile.java
@@ -3,6 +3,7 @@ package org.davidcalabrese.controller;
 import org.davidcalabrese.entity.User;
 import org.davidcalabrese.persistence.GenericDao;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -19,12 +20,20 @@ public class EditProfile extends HttpServlet  {
         String email = (String) session.getAttribute("email");
         GenericDao<User> userDao = new GenericDao<>(User.class);
 
-
         User newUser = new User(userName, email);
         newUser.setFirstName(req.getParameter("first_name"));
         newUser.setLastName(req.getParameter("last_name"));
         newUser.setSummary(req.getParameter("about"));
 
-        userDao.insert(newUser);
+        // insert user and retrieve from db
+        int id = userDao.insert(newUser);
+        User insertedUser = userDao.getById(id);
+
+        // save user in session
+        session.setAttribute("user", insertedUser);
+
+        String url = "/jsp/all_posts.jsp";
+        RequestDispatcher dispatcher = getServletContext().getRequestDispatcher(url);
+        dispatcher.forward(req, resp);
     }
 }
