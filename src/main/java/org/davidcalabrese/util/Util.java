e60c0e626d92b1add84588fb92a431fc562f02eb
David Calabrese <david.thomas.calabrese@gmail.com>
2022-03-13 20:54:43 -0500

    add static method get getting tag color from name

--- src/main/java/org/davidcalabrese/util/Util.java
+++ src/main/java/org/davidcalabrese/util/Util.java
@@ -16,4 +16,26 @@ public class Util {
         List<User> users = userDao.findByPropertyEqual("userName", userName);
         return users.get(0);
     }
+
+    public static String getCorrespondingTagColor(String tagName) {
+        String color;
+        switch(tagName) {
+            case "politics": color = "red";       break;
+            case "education": color = "pink";     break;
+            case "movies": color = "purple";      break;
+            case "books": color = "indigo";       break;
+            case "entertainment": color = "blue"; break;
+            case "food": color = "cyan";          break;
+            case "personal": color = "teal";      break;
+            case "technology": color = "green";   break;
+            case "business": color = "lime";      break;
+            case "sorts": color = "yellow";       break;
+            case "opinion": color = "amber";      break;
+            case "science": color = "orange";     break;
+            case "health": color = "brown";       break;
+            case "travel": color = "grey";        break;
+            default: color = "blue-grey";
+        }
+        return color;
+    }
 }
