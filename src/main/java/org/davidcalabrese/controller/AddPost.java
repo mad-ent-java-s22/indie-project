e8e3a3bfd10a1e52cdb00741aa88e80ce6f2f464
David Calabrese <david.thomas.calabrese@gmail.com>
2022-03-13 20:12:08 -0500

    add form handling logic

---
+++ src/main/java/org/davidcalabrese/controller/AddPost.java
@@ -0,0 +1,55 @@
+package org.davidcalabrese.controller;
+
+import org.davidcalabrese.entity.Post;
+import org.davidcalabrese.entity.Tag;
+import org.davidcalabrese.persistence.GenericDao;
+import org.davidcalabrese.util.Util;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
+
+@WebServlet(name = "AddPost", urlPatterns = { "/add_post" })
+public class AddPost extends HttpServlet  {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // get userName and email that from cognito, stored in session
+        HttpSession session = req.getSession();
+        String userName = (String) session.getAttribute("userName");
+
+        GenericDao<Post> postDao = new GenericDao<>(Post.class);
+
+        Post newPost = new Post();
+        newPost.setTitle(req.getParameter("title"));
+        newPost.setSummary(req.getParameter("summary"));
+        newPost.setContent(req.getParameter("content"));
+        newPost.setUser(Util.getUser(userName));
+        newPost.setDateCreated(LocalDate.now());
+
+        // store values from tag <select> as an array
+        String[] tagArray = req.getParameterValues("tags");
+        Set<Tag> tagSet = new HashSet<>();
+
+        // for each tag selected, create tag object and add to tagSet
+        for (String tagName : tagArray) {
+            Tag newTag = new Tag(tagName);
+            tagSet.add(newTag);
+        }
+
+        newPost.setTags(tagSet);
+
+        //get id of newly inserted post and send user to that page to see submission
+        int idOfInsertedPost = postDao.insert(newPost);
+        String url = "/jsp/posts/" + idOfInsertedPost + ".jsp";
+        RequestDispatcher dispatcher = getServletContext().getRequestDispatcher(url);
+        dispatcher.forward(req, resp);
+    }
+}
