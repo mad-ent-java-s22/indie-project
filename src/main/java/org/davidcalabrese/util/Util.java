a91cf447030053fbf9a96e8cc8c45c17233f47bf
David Calabrese <david.thomas.calabrese@gmail.com>
2022-03-13 20:14:20 -0500

    add getUser static method to util class

--- src/main/java/org/davidcalabrese/util/Util.java
+++ src/main/java/org/davidcalabrese/util/Util.java
@@ -1,8 +1,19 @@
 package org.davidcalabrese.util;
 
+import org.davidcalabrese.entity.User;
+import org.davidcalabrese.persistence.GenericDao;
+
+import java.util.List;
+
 public class Util {
     public static int getId(String pathInfo) {
         // grab everything after slash in url, should be the post, user or tag id
         return Integer.parseInt(pathInfo.substring(1));
+    }
+
+    public static User getUser(String userName) {
+        GenericDao<User> userDao = new GenericDao<>(User.class);
+        List<User> users = userDao.findByPropertyEqual("userName", userName);
+        return users.get(0);
     }
 }
