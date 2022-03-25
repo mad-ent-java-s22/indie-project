f26a7f6fd2e0875fc459d2417af02fcc7a865e11
David Calabrese <david.thomas.calabrese@gmail.com>
2022-03-25 15:48:31 -0500

    add logic to update profile

--- src/main/java/org/davidcalabrese/controller/UpdateProfile.java
+++ src/main/java/org/davidcalabrese/controller/UpdateProfile.java
@@ -1,2 +1,45 @@
-package org.davidcalabrese.controller;public class UpdateProfile {
+package org.davidcalabrese.controller;
+
+import org.davidcalabrese.entity.User;
+import org.davidcalabrese.persistence.GenericDao;
+import org.davidcalabrese.util.Util;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+import static org.davidcalabrese.util.Util.getUser;
+
+@WebServlet(name = "UpdateProfile", urlPatterns = { "/update_profile" })
+public class UpdateProfile extends HttpServlet  {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // get userName from session
+        HttpSession session = req.getSession();
+        String userName = (String) session.getAttribute("userName");
+
+        // fetch user from db
+        User user = getUser(userName);
+        GenericDao<User> userDao = new GenericDao<>(User.class);
+
+        user.setFirstName(req.getParameter("first_name"));
+        user.setLastName(req.getParameter("last_name"));
+        user.setSummary(req.getParameter("about"));
+        user.setProfileImage("default_profile_pic.jpg");
+
+        // update user in db
+        userDao.saveOrUpdate(user);
+
+        // update user in session
+        session.setAttribute("user", user);
+
+        String url = "/jsp/profile_updated.jsp";
+        RequestDispatcher dispatcher = getServletContext().getRequestDispatcher(url);
+        dispatcher.forward(req, resp);
+    }
 }
