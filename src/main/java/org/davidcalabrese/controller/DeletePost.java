484e458eb6ceb620189cfe227ca95e38e8301958
David Calabrese <david.thomas.calabrese@gmail.com>
2022-03-29 08:37:19 -0500

    create servlet for deleting a post

--- src/main/java/org/davidcalabrese/controller/DeletePost.java
+++ src/main/java/org/davidcalabrese/controller/DeletePost.java
@@ -1,2 +1,32 @@
-package org.davidcalabrese.controller;public class DeletePost {
+package org.davidcalabrese.controller;
+
+import org.davidcalabrese.entity.Post;
+import org.davidcalabrese.entity.User;
+import org.davidcalabrese.persistence.GenericDao;
+import org.davidcalabrese.util.Util;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "DeletePost", urlPatterns = { "/delete_post/*" })
+public class DeletePost extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // grab post id param from url, create post dao
+        int postId = Util.getId(req.getPathInfo());
+        GenericDao<Post> postDao = new GenericDao<>(Post.class);
+
+        Post postToDelete = postDao.getById(postId); // fetch post with id
+        postDao.delete(postToDelete); // delete post
+
+        String url = "/jsp/post_deleted.jsp";
+
+        RequestDispatcher dispatcher = getServletContext().getRequestDispatcher(url);
+        dispatcher.forward(req, resp);
+    }
 }
