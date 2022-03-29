a35efd80d78688bb4cc18ffd8e9b7e60610defc1
David Calabrese <david.thomas.calabrese@gmail.com>
2022-03-29 08:40:04 -0500

    add servlet stub for updating post

--- src/main/java/org/davidcalabrese/controller/UpdatePost.java
+++ src/main/java/org/davidcalabrese/controller/UpdatePost.java
@@ -1,2 +1,16 @@
-package org.davidcalabrese.controller;public class UpdatePost {
+package org.davidcalabrese.controller;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "UpdatePost", urlPatterns = { "/update_post" })
+public class UpdatePost extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
 }
