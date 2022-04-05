1fac2ef5d8c4bab4bc57b08b8d8274a0c6ce7a7a
David Calabrese <david.thomas.calabrese@gmail.com>
2022-04-05 15:34:19 -0500

    remove unsed getUser function, add makeTagSet

--- src/main/java/org/davidcalabrese/util/Util.java
+++ src/main/java/org/davidcalabrese/util/Util.java
@@ -1,9 +1,12 @@
 package org.davidcalabrese.util;
 
+import org.davidcalabrese.entity.Tag;
 import org.davidcalabrese.entity.User;
 import org.davidcalabrese.persistence.GenericDao;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Util {
     public static int getId(String pathInfo) {
@@ -17,9 +20,17 @@ public class Util {
         return users.get(0);
     }
 
-    public static User getUser(String userName, GenericDao<User> userDao) {
-        List<User> users = userDao.findByPropertyEqual("userName", userName);
-        return users.get(0);
+    public static Set<Tag> makeTagSet(String[] tagArray) {
+        GenericDao<Tag> tagDao = new GenericDao<>(Tag.class);
+        Set<Tag> tagSet = new HashSet<>();
+
+        // for each tag selected, create tag object and add to tagSet
+        for (String tagName : tagArray) {
+            List<Tag> tagList = tagDao.findByPropertyEqual("name", tagName);
+            Tag tag = tagList.get(0);
+            tagSet.add(tag);
+        }
+        return tagSet;
     }
 
 }
