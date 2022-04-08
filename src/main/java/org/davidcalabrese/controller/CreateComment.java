943be701422cdc9a41fcebafa0a9850d3555b4bb
David Calabrese <david.thomas.calabrese@gmail.com>
2022-04-07 20:23:54 -0500

    start createComment servlet

--- src/main/java/org/davidcalabrese/controller/CreateComment.java
+++ src/main/java/org/davidcalabrese/controller/CreateComment.java
@@ -1,2 +1,42 @@
-package org.davidcalabrese.controller;public class CreateComment {
+package org.davidcalabrese.controller;
+
+import org.davidcalabrese.entity.Comment;
+import org.davidcalabrese.entity.Post;
+import org.davidcalabrese.entity.User;
+import org.davidcalabrese.persistence.GenericDao;
+import org.davidcalabrese.util.Util;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.time.LocalDate;
+
+@WebServlet(name = "CreateComment", urlPatterns = { "/create_comment" })
+public class CreateComment extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        HttpSession session = req.getSession();
+        String userName = (String) session.getAttribute("userName");
+        User user = Util.getUser(userName);
+
+        GenericDao<Comment> commentDao = new GenericDao<>(Comment.class);
+        GenericDao<Post> postDao = new GenericDao<>(Post.class);
+
+        String commentedPostId = req.getParameter("post_id");
+        Post commentedPost = postDao.getById(Integer.parseInt(commentedPostId));
+
+        Comment comment = new Comment();
+        comment.setContent(req.getParameter("content"));
+        comment.setAuthor(user);
+        comment.setDateCreated(LocalDate.now());
+        comment.setPost(commentedPost);
+
+        String url = "/posts/" + commentedPostId;
+        getServletContext().getRequestDispatcher(url).forward(req, resp);
+    }
 }
