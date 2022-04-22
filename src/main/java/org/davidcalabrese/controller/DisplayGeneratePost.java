931532f8a74b4b113735878cb93cc475fd99bd10
David Calabrese <david.thomas.calabrese@gmail.com>
2022-04-22 11:47:28 -0500

    add servlets for making api call

--- src/main/java/org/davidcalabrese/controller/DisplayGeneratePost.java
+++ src/main/java/org/davidcalabrese/controller/DisplayGeneratePost.java
@@ -1,2 +1,40 @@
-package org.davidcalabrese.controller;public class DisplayChooseTopic {
+package org.davidcalabrese.controller;
+
+import org.davidcalabrese.entity.Post;
+import org.davidcalabrese.entity.Tag;
+import org.davidcalabrese.persistence.GenericDao;
+import org.davidcalabrese.util.Util;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+/**
+ *  Contains method for displaying a page where user can choose topic and generate post
+ */
+@WebServlet(name = "DisplayGeneratePost", urlPatterns = { "/display_generate_post" })
+public class DisplayGeneratePost extends HttpServlet {
+    /**
+     * Called by server to allow servlet to handle a GET request
+     *
+     * @param req               object containing req client has made of the servlet
+     * @param resp              object that containing resp servlet sends to the client
+     * @throws ServletException if an input or output error is detected when handling GET req
+     * @throws IOException      if the request for the GET could not be handled
+     */
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        GenericDao<Tag> tagDao = new GenericDao<>(Tag.class);
+        List<Tag> tagNames  = tagDao.getAll();
+
+        req.setAttribute("tagNames", tagNames);
+        String url = "/jsp/generate_post.jsp";
+        RequestDispatcher dispatcher = getServletContext().getRequestDispatcher(url);
+        dispatcher.forward(req, resp);
+    }
 }
--- src/main/java/org/davidcalabrese/controller/GeneratePost.java
+++ src/main/java/org/davidcalabrese/controller/GeneratePost.java
@@ -1,2 +1,61 @@
-package org.davidcalabrese.controller;public class GeneratePost {
+package org.davidcalabrese.controller;
+
+import org.davidcalabrese.entity.OpenAIResponse;
+import org.davidcalabrese.entity.Tag;
+import org.davidcalabrese.persistence.GenericDao;
+import org.davidcalabrese.services.OpenAIService;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ *  Calls OpenAI API to get a blog post on topic
+ */
+@WebServlet(name = "GeneratePost", urlPatterns = { "/generate_post" })
+public class GeneratePost extends HttpServlet {
+    /**
+     * Called by server to allow servlet to handle a GET request
+     *
+     * @param req               object containing req client has made of the servlet
+     * @param resp              object that containing resp servlet sends to the client
+     * @throws ServletException if an input or output error is detected when handling GET req
+     * @throws IOException      if the request for the GET could not be handled
+     */
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        GenericDao<Tag> tagDao = new GenericDao<>(Tag.class);
+        String[] tagArray = req.getParameterValues("tags");
+
+        List<String> tagList = new ArrayList<>(Arrays.asList(tagArray));
+        List<Tag> possibleTags  = tagDao.getAll();
+
+        OpenAIService service = new OpenAIService();
+
+        OpenAIResponse response = null;
+        try {
+            response = service.getPostOnTopic(tagList);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+
+        assert response != null;
+        String postText = response.getChoices().get(0).getText();
+
+        req.setAttribute("postText", postText);
+        req.setAttribute("possibleTags", possibleTags);
+        req.setAttribute("chosenTags", tagList);
+
+        String url = "/jsp/confirm_generated_post.jsp";
+        RequestDispatcher dispatcher = getServletContext().getRequestDispatcher(url);
+        dispatcher.forward(req, resp);
+    }
 }
