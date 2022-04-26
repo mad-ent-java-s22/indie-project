ab33f64a06f61bff88bfb6bb03dac5a0026a4776
David Calabrese <david.thomas.calabrese@gmail.com>
2022-04-26 17:43:50 -0500

    add servlet logic to route to profile jsp and display form

--- src/main/java/org/davidcalabrese/controller/DisplayEditProfile.java
+++ src/main/java/org/davidcalabrese/controller/DisplayEditProfile.java
@@ -1,4 +1,28 @@
 package org.davidcalabrese.controller;
 
-public class DisplayProfile {
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ *  Contains method for displaying the profile form
+ */
+@WebServlet(name = "DisplayEditProfile", urlPatterns = { "/display_edit_profile" })
+public class DisplayEditProfile extends HttpServlet {
+    /**
+     * Called by server to allow servlet to handle a GET request
+     *
+     * @param req               object containing req client has made of the servlet
+     * @param resp              object that containing resp servlet sends to the client
+     * @throws ServletException if an input or output error is detected when handling GET req
+     * @throws IOException      if the request for the GET could not be handled
+     */
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setAttribute("showProfileForm", true);
+        req.getRequestDispatcher("/jsp/profile.jsp").forward(req, resp);
+    }
 }
