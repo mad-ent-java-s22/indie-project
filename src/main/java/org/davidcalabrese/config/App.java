ac14f1f4fe67e3a3a2075f8e0fec951c87e87d13
David Calabrese <david.thomas.calabrese@gmail.com>
2022-04-28 11:28:50 -0500

    add doc comments to config

--- src/main/java/org/davidcalabrese/config/App.java
+++ src/main/java/org/davidcalabrese/config/App.java
@@ -7,8 +7,10 @@ import javax.ws.rs.core.Application;
 import java.util.HashSet;
 import java.util.Set;
 
-//Defines the base URI for all resource URIs.
-@ApplicationPath("/api") //You may want to add a value here so that all traffic isn't routed to the class below.
+/**
+ * Configures the openAI API
+ */
+@ApplicationPath("/api")
 //The java class declares root resource and provider classes
 public class App extends Application {
     //The method returns a non-empty collection with classes, that must be included in the published JAX-RS application
